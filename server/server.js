bb638a4755961ec2d3b8c2b6cb4ceaeb9ab1ac09
davidfred1994 <davidfred1994@gmail.com>
2019-05-03 15:10:23 -0700

    fixed npm and ran a few

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 const router = require('./router.js');
 
 const app = express();
-app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, '../public')));
