476f3b37413bf31db101a354ecf5c89fc9a9bf58
davidfred1994 <davidfred1994@gmail.com>
2019-05-04 10:41:57 -0700

    morgan added for debug

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const router = require('./router.js');
+const morgan = require('morgan')
 
 const app = express();
+app.use(morgan('dev'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, '../public')));
