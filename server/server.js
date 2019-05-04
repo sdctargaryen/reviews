13941f3fb541ef4e3d160a2cd250261470386a5d
davidfred1994 <davidfred1994@gmail.com>
2019-05-04 10:49:15 -0700

    debug good taking off morgan

--- server/server.js
+++ server/server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const router = require('./router.js');
-const morgan = require('morgan')
 
 const app = express();
-app.use(morgan('dev'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, '../public')));
